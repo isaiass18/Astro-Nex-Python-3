ec6c1a1b063a71beb55cb6e3535202e4bbcb5b12
isaiass18 <47506189+isaiass18@users.noreply.github.com>
2026-07-24 08:31:33 -0500

    Rebuild Windows installer from current sources

--- Windows Instalador/Astro-Nex/_internal/astronex/scripts/pngplangram.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from .scripts import Script
-       
-class PngPlangram(Script):
-    def run(self,*arg,**kw):
-        from .. surfaces.pngsurface import DrawPng
-        DrawPng.simple_batch()
-
-pngplangram = PngPlangram 
--- Windows Instalador/Astro-Nex/_internal/astronex/scripts/scripts.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-class Script(object):
-    def __init__(self,boss):
-        self.boss = boss
-
-    def run(self,*ag,**kw):
-        pass
-
