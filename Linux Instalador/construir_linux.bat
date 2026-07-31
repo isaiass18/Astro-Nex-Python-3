6c99954dd2dc48a3b8b03115dae22314336b436e
isaiass18 <isaiass18@users.noreply.github.com>
2026-07-31 06:05:11 -0500

    Actualiza scripts de Linux para descargar ambos archivos (deb y appimage) a la vez en la opción 1

--- Linux Instalador/construir_linux.bat
+++ Linux Instalador/construir_linux.bat
@@ -1,5 +1,5 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
 set AWS_IP=3.16.216.254
 set AWS_USER=ubuntu
@@ -8,11 +8,10 @@ set PEM_FILE=..\astronext.pem
 echo =========================================
 echo    Constructor de Astro-Nex para Linux   
 echo =========================================
-echo 1. Construir .deb para Ubuntu 22.04
-echo 2. Construir .deb para Ubuntu 24.04
-echo 3. Construir AppImage Universal
+echo 1. Construir .deb Universal (basado en AppImage)
+echo 2. Construir AppImage Universal
 echo =========================================
-set /p OPCION="Elige una opcion (1-3): "
+set /p OPCION="Elige una opcion (1-2): "
 
 if not exist "%PEM_FILE%" (
     echo [ERROR] No se encuentra la llave SSH %PEM_FILE%.
@@ -21,14 +20,14 @@ if not exist "%PEM_FILE%" (
 )
 
 if "%OPCION%"=="1" (
-    set TARGET_FILE=Astro-Nex-v2.0-beta-Ubuntu22.04-amd64.deb
-    set BUILD_CMD=sudo docker build --build-arg UBUNTU_VER=22.04 -t astronex-builder -f 'Linux Instalador/Dockerfile.deb' .
-) else if "%OPCION%"=="2" (
     set TARGET_FILE=Astro-Nex-v2.0-beta-Ubuntu24.04-amd64.deb
-    set BUILD_CMD=sudo docker build --build-arg UBUNTU_VER=24.04 -t astronex-builder -f 'Linux Instalador/Dockerfile.deb' .
-) else if "%OPCION%"=="3" (
+    set APPIMAGE_FILE=Astro-Nex-v2.0-beta-Linux-x86_64.AppImage
+    set BUILD_CMD=sudo docker build -t astronex-builder -f "Linux Instalador/Dockerfile.appimage" . ^&^& sudo docker build -t astronex-deb-builder -f "Linux Instalador/Dockerfile.deb" .
+    set EXTRACT_CMD=sudo docker create --name temp-container-deb astronex-deb-builder ^&^& sudo docker cp temp-container-deb:/!TARGET_FILE! . ^&^& sudo docker rm temp-container-deb ^&^& sudo docker create --name temp-container-appimage astronex-builder ^&^& sudo docker cp temp-container-appimage:/!APPIMAGE_FILE! . ^&^& sudo docker rm temp-container-appimage
+) else if "%OPCION%"=="2" (
     set TARGET_FILE=Astro-Nex-v2.0-beta-Linux-x86_64.AppImage
-    set BUILD_CMD=sudo docker build -t astronex-builder -f 'Linux Instalador/Dockerfile.appimage' .
+    set BUILD_CMD=sudo docker build -t astronex-builder -f "Linux Instalador/Dockerfile.appimage" .
+    set EXTRACT_CMD=sudo docker create --name temp-container astronex-builder ^&^& sudo docker cp temp-container:/!TARGET_FILE! . ^&^& sudo docker rm temp-container
 ) else (
     echo Opcion no valida.
     pause
@@ -48,7 +47,7 @@ echo.
 echo [*] Descargando codigo fresco y compilando (esto tomara varios minutos)...
 ssh -i "%PEM_FILE%" -o IdentitiesOnly=yes %AWS_USER%@%AWS_IP% "sudo docker system prune -f && rm -rf astronex-build-tmp && mkdir astronex-build-tmp && cd astronex-build-tmp && git clone https://github.com/isaiass18/Astro-Nex-Python-3.git ."
 scp -i "%PEM_FILE%" -r -o IdentitiesOnly=yes -o StrictHostKeyChecking=no "..\Linux Instalador" "%AWS_USER%@%AWS_IP%:~/astronex-build-tmp/"
-ssh -i "%PEM_FILE%" -o IdentitiesOnly=yes %AWS_USER%@%AWS_IP% "cd astronex-build-tmp && %BUILD_CMD% && sudo docker create --name temp-container astronex-builder && sudo docker cp temp-container:/%TARGET_FILE% . && sudo docker rm temp-container"
+ssh -i "%PEM_FILE%" -o IdentitiesOnly=yes %AWS_USER%@%AWS_IP% "cd astronex-build-tmp && %BUILD_CMD% && %EXTRACT_CMD%"
 if %errorlevel% neq 0 (
     echo [ERROR] Fallo la compilacion en AWS.
     pause
@@ -56,8 +55,13 @@ if %errorlevel% neq 0 (
 )
 
 echo.
-echo [*] Descargando el archivo %TARGET_FILE% al equipo local...
-scp -i "%PEM_FILE%" -o IdentitiesOnly=yes %AWS_USER%@%AWS_IP%:/home/%AWS_USER%/astronex-build-tmp/%TARGET_FILE% .\%TARGET_FILE%
+echo [*] Descargando los instaladores al equipo local...
+if "%OPCION%"=="1" (
+    scp -i "%PEM_FILE%" -o IdentitiesOnly=yes %AWS_USER%@%AWS_IP%:/home/%AWS_USER%/astronex-build-tmp/%TARGET_FILE% .\%TARGET_FILE%
+    scp -i "%PEM_FILE%" -o IdentitiesOnly=yes %AWS_USER%@%AWS_IP%:/home/%AWS_USER%/astronex-build-tmp/%APPIMAGE_FILE% .\%APPIMAGE_FILE%
+) else (
+    scp -i "%PEM_FILE%" -o IdentitiesOnly=yes %AWS_USER%@%AWS_IP%:/home/%AWS_USER%/astronex-build-tmp/%TARGET_FILE% .\%TARGET_FILE%
+)
 
 echo.
 echo [*] Limpiando el servidor AWS...
--- Linux Instalador/construir_linux.sh
+++ Linux Instalador/construir_linux.sh
@@ -22,12 +22,13 @@ chmod 400 "$PEM_FILE"
 
 if [ "$OPCION" = "1" ]; then
     TARGET_FILE="Astro-Nex-v2.0-beta-Ubuntu24.04-amd64.deb"
+    APPIMAGE_FILE="Astro-Nex-v2.0-beta-Linux-x86_64.AppImage"
     BUILD_CMD="sudo docker build -t astronex-builder -f 'Linux Instalador/Dockerfile.appimage' . && sudo docker build -t astronex-deb-builder -f 'Linux Instalador/Dockerfile.deb' ."
-    CONTAINER_NAME="astronex-deb-builder"
+    EXTRACT_CMD="sudo docker create --name temp-container-deb astronex-deb-builder && sudo docker cp temp-container-deb:/$TARGET_FILE . && sudo docker rm temp-container-deb && sudo docker create --name temp-container-appimage astronex-builder && sudo docker cp temp-container-appimage:/$APPIMAGE_FILE . && sudo docker rm temp-container-appimage"
 elif [ "$OPCION" = "2" ]; then
     TARGET_FILE="Astro-Nex-v2.0-beta-Linux-x86_64.AppImage"
     BUILD_CMD="sudo docker build -t astronex-builder -f 'Linux Instalador/Dockerfile.appimage' ."
-    CONTAINER_NAME="astronex-builder"
+    EXTRACT_CMD="sudo docker create --name temp-container astronex-builder && sudo docker cp temp-container:/$TARGET_FILE . && sudo docker rm temp-container"
 else
     echo "Opción no válida."
     exit 1
@@ -41,11 +42,16 @@ echo ""
 echo "[*] Descargando código fresco y compilando (esto tomará varios minutos)..."
 ssh -i "$PEM_FILE" -o IdentitiesOnly=yes "$AWS_USER@$AWS_IP" "sudo docker system prune -f && rm -rf astronex-build-tmp && mkdir astronex-build-tmp && cd astronex-build-tmp && git clone https://github.com/isaiass18/Astro-Nex-Python-3.git ."
 scp -i "$PEM_FILE" -r -o IdentitiesOnly=yes -o StrictHostKeyChecking=no "../Linux Instalador" "$AWS_USER@$AWS_IP:~/astronex-build-tmp/"
-ssh -i "$PEM_FILE" -o IdentitiesOnly=yes "$AWS_USER@$AWS_IP" "cd astronex-build-tmp && $BUILD_CMD && sudo docker create --name temp-container $CONTAINER_NAME && sudo docker cp temp-container:/$TARGET_FILE . && sudo docker rm temp-container"
+ssh -i "$PEM_FILE" -o IdentitiesOnly=yes "$AWS_USER@$AWS_IP" "cd astronex-build-tmp && $BUILD_CMD && $EXTRACT_CMD"
 
 echo ""
-echo "[*] Descargando el archivo $TARGET_FILE al equipo local..."
-scp -i "$PEM_FILE" -o IdentitiesOnly=yes "$AWS_USER@$AWS_IP:/home/$AWS_USER/astronex-build-tmp/$TARGET_FILE" "./$TARGET_FILE"
+echo "[*] Descargando los instaladores al equipo local..."
+if [ "$OPCION" = "1" ]; then
+    scp -i "$PEM_FILE" -o IdentitiesOnly=yes "$AWS_USER@$AWS_IP:/home/$AWS_USER/astronex-build-tmp/$TARGET_FILE" "./$TARGET_FILE"
+    scp -i "$PEM_FILE" -o IdentitiesOnly=yes "$AWS_USER@$AWS_IP:/home/$AWS_USER/astronex-build-tmp/$APPIMAGE_FILE" "./$APPIMAGE_FILE"
+else
+    scp -i "$PEM_FILE" -o IdentitiesOnly=yes "$AWS_USER@$AWS_IP:/home/$AWS_USER/astronex-build-tmp/$TARGET_FILE" "./$TARGET_FILE"
+fi
 
 echo ""
 echo "[*] Limpiando el servidor AWS..."
